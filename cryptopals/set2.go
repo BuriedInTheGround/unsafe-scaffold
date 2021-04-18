02d5b8cb8a2e64dc0c43efdc449dc04e070d7999
Simone Ragusa <simone99.as@gmail.com>
2021-04-18 17:47:21 +0200

    cryptopals: add set 2, challenges 11, 12 + fix CBC enc/dec
    Encryption in CBC mode wasn't actually working, now it's fixed.
    Decryption in CBC mode had a flaw in remove padding in the case of a
    block-wide padding of zeros. Fixed.

--- cryptopals/set2.go
+++ cryptopals/set2.go
@@ -1,6 +1,12 @@
 package cryptopals
 
-import "crypto/cipher"
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"math/big"
+)
 
 func padPKCS7(in []byte, blockSize int) []byte {
 	if blockSize <= 0 {
@@ -40,7 +46,7 @@ func encryptCBC(iv, in []byte, b cipher.Block) []byte {
 		if i == 0 {
 			t = xor(iv, plaintext[i:i+b.BlockSize()])
 		} else {
-			t = xor(res[i-b.BlockSize():i], plaintext)
+			t = xor(res[i-b.BlockSize():i], plaintext[i:i+b.BlockSize()])
 		}
 		copy(res[i:i+b.BlockSize()], encryptECB(t, b))
 	}
@@ -64,5 +70,116 @@ func decryptCBC(iv, in []byte, b cipher.Block) []byte {
 		}
 	}
 	padLen := int(res[len(res)-1])
+	if padLen == 0 {
+		return res[:len(res)-b.BlockSize()]
+	}
 	return res[:len(res)-padLen]
+}
+
+func randomBytes(n int) []byte {
+	res := make([]byte, n)
+	b, err := rand.Read(res)
+	if err != nil {
+		panic("randomBytes: failed to read from random")
+	}
+	return res[:b]
+}
+
+func encryptWithRandomKey(in []byte) []byte {
+	key := randomBytes(aes.BlockSize)
+	b, err := aes.NewCipher(key)
+	if err != nil {
+		panic("encryptWithRandomKey: cannot initialize cipher")
+	}
+
+	// Append 5-10 random bytes before the plaintext.
+	rndBefore, err := rand.Int(rand.Reader, big.NewInt(6))
+	panicIfErr(err)
+	bytesBefore := int(rndBefore.Int64()) + 5
+	in = append(randomBytes(bytesBefore), in...)
+
+	// Append 5-10 random bytes after the plaintext.
+	rndAfter, err := rand.Int(rand.Reader, big.NewInt(6))
+	panicIfErr(err)
+	bytesAfter := int(rndAfter.Int64()) + 5
+	in = append(in, randomBytes(bytesAfter)...)
+
+	// Choose which encryption mode to use.
+	var encryptWithCBC bool
+	coinFlip, err := rand.Int(rand.Reader, big.NewInt(2))
+	panicIfErr(err)
+	if coinFlip.Int64() > 0 {
+		encryptWithCBC = true
+	}
+
+	if encryptWithCBC {
+		return encryptCBC(randomBytes(b.BlockSize()), in, b)
+	}
+	return encryptECB(padPKCS7(in, b.BlockSize()), b)
+}
+
+func panicIfErr(err error) {
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func newECBSuffixOracle(secretSuffix []byte) func([]byte) []byte {
+	key := randomBytes(aes.BlockSize)
+	b, err := aes.NewCipher(key)
+	if err != nil {
+		panic("newECBSuffixOracle: cannot initialize cipher")
+	}
+	return func(in []byte) []byte {
+		in = append(in, secretSuffix...)
+		return encryptECB(padPKCS7(in, b.BlockSize()), b)
+	}
+}
+
+func recoverECBSuffix(oracle func([]byte) []byte) []byte {
+	blockSize := len(oracle(bytes.Repeat([]byte("A"), 1)))
+	for i := 2; i <= 128; i++ {
+		ct := oracle(bytes.Repeat([]byte("A"), i))
+		if len(ct) > blockSize {
+			blockSize = len(ct) - blockSize
+			break
+		}
+	}
+
+	if !detectECB(oracle(bytes.Repeat([]byte("magic"), 64))) {
+		panic("recoverECBSuffix: oracle is not using ECB")
+	}
+
+	var suffix []byte
+
+	// Iterate j times, up to a maximum of 100 blocks.
+	for j := 1; j < 100; j++ {
+		for b := 1; b < blockSize; b++ {
+			// Evaluate the target, which actually is the last byte of the
+			// output of `oracle(known-string || unknown-1-byte)`.
+			target := oracle(bytes.Repeat([]byte("A"), j*blockSize-len(suffix)-1))
+			target = append(target, suffix...)
+
+			// Store every possible last byte oracle output.
+			dict := make(map[string]byte)
+			for i := 0; i < 256; i++ {
+				payload := bytes.Repeat([]byte("A"), j*blockSize-len(suffix)-1)
+				payload = append(payload, suffix...)
+				payload = append(payload, byte(i))
+				out := oracle(payload)
+				dict[string(out[(j-1)*blockSize:j*blockSize])] = byte(i)
+			}
+
+			// Find the one byte that match and append it to the recovered
+			// suffix. If the matching byte has value less than blockSize (and
+			// different from newline) terminate, as that is padding whp.
+			suffixByte := dict[string(target[(j-1)*blockSize:j*blockSize])]
+			if suffixByte != byte('\n') && suffixByte < byte(blockSize) {
+				break
+			}
+			suffix = append(suffix, suffixByte)
+		}
+	}
+
+	return suffix
 }
--- cryptopals/set2_test.go
+++ cryptopals/set2_test.go
@@ -3,6 +3,7 @@ package cryptopals
 import (
 	"bytes"
 	"crypto/aes"
+	"math"
 	"testing"
 )
 
@@ -41,4 +42,35 @@ func TestChallenge10(t *testing.T) {
 	iv := bytes.Repeat([]byte{0}, b.BlockSize())
 	message := decryptCBC(iv, input, b)
 	t.Logf("decrypted message =\n%s", message)
+}
+
+func TestChallenge11(t *testing.T) {
+	input := bytes.Repeat([]byte("magic"), 64)
+
+	var ecb, cbc int
+	for i := 0; i < 2000; i++ {
+		out := encryptWithRandomKey(input)
+		if detectECB(out) {
+			ecb++
+		} else {
+			cbc++
+		}
+	}
+
+	if math.Abs(float64(ecb-cbc)) > 100 {
+		t.Fatalf("ECB and CBC encryption times differ by more than 5%%; got ecb=%d, cbc=%d", ecb, cbc)
+	}
+}
+
+func TestChallenge12(t *testing.T) {
+	want := base64ToByteSlice(t, `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
+aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
+dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
+YnkK`)
+	oracle := newECBSuffixOracle(want)
+	got := recoverECBSuffix(oracle)
+	t.Logf("secret suffix recovered =\n%s", got)
+	if !bytes.Equal(got, want) {
+		t.Fatalf("wrong suffix; want %q, got %q", want, got)
+	}
 }
